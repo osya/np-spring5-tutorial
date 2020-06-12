ab8a84d688aa8c56980099944420be6803a08fd5
Valeriy Osipov <info@vosipov.com>
2020-06-12 17:54:55 +0300

    Add HelloController

--- src/main/java/com/vosipov/npspring5tutorial/NpSpring5TutorialApplication.java
+++ src/main/java/com/vosipov/npspring5tutorial/NpSpring5TutorialApplication.java
@@ -2,20 +2,11 @@ package com.vosipov.npspring5tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class NpSpring5TutorialApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NpSpring5TutorialApplication.class, args);
-    }
-
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
     }
 }
