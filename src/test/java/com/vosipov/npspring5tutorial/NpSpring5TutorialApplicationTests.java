c0d61c9fabb62a9aba3bdd8031c11a8b120c2093
Valeriy Osipov <info@vosipov.com>
2020-06-12 14:38:07 +0300

    Initial commit

---
+++ src/test/java/com/vosipov/npspring5tutorial/NpSpring5TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vosipov.npspring5tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpSpring5TutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
