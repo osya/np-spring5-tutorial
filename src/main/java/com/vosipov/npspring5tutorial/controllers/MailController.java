42b9a85de994f71576beefadad22e636d2daeecc
Valeriy Osipov <info@vosipov.com>
2020-06-13 18:26:06 +0300

    Add bean name

--- src/main/java/com/vosipov/npspring5tutorial/controllers/MailController.java
+++ src/main/java/com/vosipov/npspring5tutorial/controllers/MailController.java
@@ -1,16 +1,17 @@
 package com.vosipov.npspring5tutorial.controllers;
 
 import com.vosipov.npspring5tutorial.mail.MailSender;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MailController {
-
-    @Autowired
     private MailSender mailSender;
+
+    public MailController(MailSender smtp) {
+        this.mailSender = smtp;
+    }
 
     @GetMapping("/mail")
     public String mail(@RequestParam(value = "myName", defaultValue = "World") String name) {
