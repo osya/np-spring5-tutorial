3d52ef941dda9d5acdcf98b09dfae8535c735ee3
Valeriy Osipov <info@vosipov.com>
2020-06-14 14:44:19 +0300

    Add @Conditional

--- src/main/java/com/vosipov/npspring5tutorial/controllers/HelloController.java
+++ src/main/java/com/vosipov/npspring5tutorial/controllers/HelloController.java
@@ -1,6 +1,5 @@
 package com.vosipov.npspring5tutorial.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${app.name}")
-    private String appName;
-
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s! %s", name, appName);
+        return String.format("Hello %s!", name);
     }
 }
--- src/main/java/com/vosipov/npspring5tutorial/mail/MailConfig.java
+++ src/main/java/com/vosipov/npspring5tutorial/mail/MailConfig.java
@@ -1,19 +1,19 @@
 package com.vosipov.npspring5tutorial.mail;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MailConfig {
     @Bean
-    @Profile("dev")
+    @ConditionalOnProperty(name="spring.mail.host", havingValue="foo", matchIfMissing=true)
     public MailSender mockMailSender() {
         return new MockMailSender();
     }
 
     @Bean
-    @Profile("!dev")
+    @ConditionalOnProperty("spring.mail.host")
     public MailSender smtpMailSender() {
         return new SmtpMailSender();
     }
