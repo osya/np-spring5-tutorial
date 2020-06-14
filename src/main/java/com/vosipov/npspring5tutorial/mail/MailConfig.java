8b1ee030583e48170a175be47ccb89ee0407a801
Valeriy Osipov <info@vosipov.com>
2020-06-14 13:41:03 +0300

    Added profiles

--- src/main/java/com/vosipov/npspring5tutorial/mail/MailConfig.java
+++ src/main/java/com/vosipov/npspring5tutorial/mail/MailConfig.java
@@ -2,15 +2,18 @@ package com.vosipov.npspring5tutorial.mail;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MailConfig {
     @Bean
+    @Profile("dev")
     public MailSender mockMailSender() {
         return new MockMailSender();
     }
 
     @Bean
+    @Profile("!dev")
     public MailSender smtpMailSender() {
         return new SmtpMailSender();
     }
